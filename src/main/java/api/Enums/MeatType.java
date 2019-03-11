a954546e2eeaa3538923785ba00cfdefbcbcc10d
barnadenes <nyukk1007@gmail.com>
2019-03-11 13:41:59 +0100

    New Enum added

---
+++ src/main/java/api/Enums/MeatType.java
@@ -0,0 +1,13 @@
+package api.Enums;
+
+public enum  MeatType {
+    RED("Red Meat"),
+    WHITE("White Meat");
+
+    private String type;
+
+    MeatType(String type) {
+        this.type = type;
+    }
+
+}
--- src/main/java/api/Enums/Portion.java
+++ src/main/java/api/Enums/Portion.java
@@ -1,4 +1,17 @@
 package api.Enums;
 
 public enum Portion {
+    LARGE(800),
+    MEDIUM(400),
+    SMALL(250);
+
+    private int calories;
+
+    Portion(int calories) {
+        this.calories = calories;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
 }
